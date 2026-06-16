bb6129cfc9810f6dd59f1d18d78687df008de8c3
arundas <arundas.sdl@gmail.com>
2026-06-16 22:17:23 +0530

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      "@phosphor-icons/react",
+      "recharts",
+      "@mui/material",
+      "@mui/icons-material",
+    ],
+  },
+};
 
 export default nextConfig;
